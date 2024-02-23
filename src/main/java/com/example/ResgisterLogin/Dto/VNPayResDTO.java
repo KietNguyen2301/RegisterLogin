95123e4da600f8bef678c42522d8bc10ffda1019
KietNguyen2301 <kietde160233@gmail.com>
2024-02-23 12:09:17 +0700

    vnpay

---
+++ src/main/java/com/example/ResgisterLogin/Dto/VNPayResDTO.java
@@ -0,0 +1,34 @@
+package com.example.ResgisterLogin.Dto;
+
+import java.io.Serializable;
+
+
+public class VNPayResDTO {
+	private String status;
+	private String message;
+	private String URL;
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getURL() {
+		return URL;
+	}
+	public void setURL(String uRL) {
+		URL = uRL;
+	}
+	
+	
+	
+	
+
+}
